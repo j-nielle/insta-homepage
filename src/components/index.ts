d90ad5208aa9149f38b5f11eb9c43b4aeedbd4f5
j-nielle <rabayaangel@gmail.com>
2025-01-28 17:38:40 +0800

    chore: setting up components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { default as MobileMockup } from './MobileMockup';
+export { default as LoginForm } from './LoginForm';
+export { default as ThemeSwitcher } from './ThemeSwitcher';
