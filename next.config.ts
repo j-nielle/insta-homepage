6108b3409ddb740e6b06221161405b84ae4832d8
j-nielle <rabayaangel@gmail.com>
2025-01-29 12:16:54 +0800

    chore: updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.instagram.com',
+        port: '',
+        pathname: '/static/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.cdninstagram.com',
+        port: '',
+        pathname: '/rsrc.php/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
